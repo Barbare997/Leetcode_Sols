7d219efa93d995c020d3f29d82ed7c390436407c
Barbare997 <barbarepantskhava@gmail.com>
2026-02-24 16:06:04 +0400

    Added Same Tree solution

--- src/TreeSolutions.java
+++ src/TreeSolutions.java
@@ -1,7 +1,19 @@
 public class TreeSolutions {
+    //Maximum Depth of Binary Tree
     public int maxDepth(TreeNode root) {
         if (root==null)
             return 0;
         return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    //Same Tree
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p==null && q==null)
+            return true;
+        else if (p==null)
+            return false;
+        else if (q==null)
+            return false;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
