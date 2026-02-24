5da012c33658ad74086a714c909de594fbfb71df
Barbare997 <barbarepantskhava@gmail.com>
2026-02-24 17:52:51 +0400

    Added isSymmetric second solution without extra space

--- src/TreeSolutions.java
+++ src/TreeSolutions.java
@@ -30,4 +30,19 @@ public class TreeSolutions {
             return true;
         return isSameTree(invertTree(root.right), root.left);
     }
+
+    public boolean isSymmetric1 (TreeNode root) {
+        if (root==null)
+            return true;
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror (TreeNode left, TreeNode right) {
+        if (left==null && right==null)
+            return true;
+        else if (left==null || right==null)
+            return false;
+        return left.val==right.val && isMirror(left.left, right.right)
+                && isMirror(left.right, right.left);
+    }
 }
