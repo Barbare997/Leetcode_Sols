d35a0f7a2d95191a52678c856d5513a5fcf870d0
Barbare997 <barbarepantskhava@gmail.com>
2026-02-26 22:54:29 +0400

    Added Pascal's Triangle solution

--- src/DPSolutions.java
+++ src/DPSolutions.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DPSolutions {
     //Climbing Stairs
     public int climbStairs(int n) {
@@ -10,6 +13,22 @@ public class DPSolutions {
             c=a+b;
         }
         return c;
+    }
+
+    //Pascal's Triangle
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i=0; i<numRows; i++) {
+            List<Integer> triangleRow = new ArrayList<>();
+            triangleRow.add(1);
+            for (int j=1; j<i; j++) {
+                triangleRow.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+            }
+            if (i>0)
+                triangleRow.add(1);
+            result.add(triangleRow);
+        }
+        return result;
     }
 
 }
