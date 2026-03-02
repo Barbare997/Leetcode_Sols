bf781b2f161c22a51eae653bb75b56e0c1cd1eda
Barbare997 <barbarepantskhava@gmail.com>
2026-03-02 18:17:27 +0400

    Added Rotate Array solution, O(n) extra space, not as optimal as O(1)

--- src/ArraySolutions.java
+++ src/ArraySolutions.java
@@ -224,4 +224,13 @@ public class ArraySolutions {
         }
         return candidate;
     }
+
+    //Rotate Array
+    public void rotate(int[] nums, int k) {
+        int[] result = new int[nums.length];
+        for (int i=0; i<nums.length; i++) {
+            result[(i+k)%nums.length] = nums[i];
+        }
+        System.arraycopy(result, 0, nums, 0, nums.length);
+    }
 }
