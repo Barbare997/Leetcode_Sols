5260fde3f40d0aa50cdbf301d32a4481347c2e52
Barbare997 <barbarepantskhava@gmail.com>
2026-03-04 16:08:18 +0400

    Added Ransom Note solution using HashMap

--- src/HashMapSolutions.java
+++ src/HashMapSolutions.java
@@ -52,4 +52,19 @@ public class HashMapSolutions {
         }
         return 0;
     }
+
+    //Ransom Note
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i=0; i<magazine.length(); i++)
+            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0)+1);
+
+        for (int i=0; i<ransomNote.length(); i++){
+            if (map.getOrDefault(ransomNote.charAt(i), 0)==0)
+                return false;
+            else map.put(ransomNote.charAt(i), map.getOrDefault(ransomNote.charAt(i), 0)-1);
+        }
+        return true;
+    }
 }
