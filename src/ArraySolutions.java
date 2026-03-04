6be9854bf50b4da3e9cf5c638ac98fe6255aaad2
Barbare997 <barbarepantskhava@gmail.com>
2026-03-04 14:42:45 +0400

    Added Best Time to Buy and Sell Stock II, easier solution, greedy

--- src/ArraySolutions.java
+++ src/ArraySolutions.java
@@ -181,6 +181,17 @@ public class ArraySolutions {
         return maxProfit;
     }
 
+    //Best Time to Buy and Sell Stock II, easier
+    public int maxProfit2(int[] prices) {
+        int maxProfit = 0;
+        for (int i=1; i<prices.length; i++) {
+            if (prices[i]>prices[i-1]) {
+                maxProfit+=prices[i]-prices[i-1];
+            }
+        }
+        return maxProfit;
+    }
+
     //Product of Array Except Self,  O(n) time, O(n) space
     public int[] productExceptSelf(int[] nums) {
         int[] prefixProduct = nums.clone();
