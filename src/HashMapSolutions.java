8c76a06506a682a153d7aaf3a05aeebd81516bd1
Barbare997 <barbarepantskhava@gmail.com>
2026-03-06 20:23:25 +0400

    Added Word Pattern solution

--- src/HashMapSolutions.java
+++ src/HashMapSolutions.java
@@ -96,4 +96,29 @@ public class HashMapSolutions {
         }
         return false;
     }
+
+    //Word Pattern
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+
+        if (pattern.length() != arr.length) return false;
+
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
+
+        for (int i=0; i<pattern.length(); i++) {
+            if (map1.containsKey(pattern.charAt(i)))
+                if (!Objects.equals(map1.get(pattern.charAt(i)), arr[i]))
+                    return false;
+            map1.put(pattern.charAt(i), arr[i]);
+        }
+
+        for (int i=0; i<arr.length; i++) {
+            if (map2.containsKey(arr[i]))
+                if (map2.get(arr[i])!= pattern.charAt(i))
+                    return false;
+            map2.put(arr[i], pattern.charAt(i));
+        }
+        return true;
+    }
 }
