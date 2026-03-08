c5662fbc1bcd76aca7fd0612c5f3ba7e5240a5a7
Barbare997 <barbarepantskhava@gmail.com>
2026-03-08 23:19:40 +0400

    Added H-Index O(n) time complexity solution using Bucket Counting/ Counting Sorting

--- src/BucketCounting.java
+++ src/BucketCounting.java
@@ -1,2 +1,22 @@
 public class BucketCounting {
+    //H-Index
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] countingArray = new int[n+1];
+
+        for (int citation: citations) {
+            if (citation>=n)
+                countingArray[n]++;
+            else
+                countingArray[citation]++;
+        }
+
+        int numOfPapers=0;
+        for (int h = n; h>=0; h--) {
+            numOfPapers+=countingArray[h];
+            if (numOfPapers>=h)
+                return h;
+        }
+        return 0;
+    }
 }
