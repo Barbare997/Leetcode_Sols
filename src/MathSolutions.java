21eed7ea0e6d7b25920a77d395c4dac332bf3460
Barbare997 <barbarepantskhava@gmail.com>
2026-03-11 23:50:50 +0400

    Added Number of Common Factors solution

--- src/MathSolutions.java
+++ src/MathSolutions.java
@@ -177,4 +177,14 @@ public int fib(int n) {
         else return n;
     }
 
+    //Number of Common Factors
+    public int commonFactors(int a, int b) {
+        int count = 0 ;
+        for (int i=1; i<=a && i<=b; i++) {
+            if (a%i==0 & b%i==0)
+                count++;
+        }
+        return count;
+    }
+
 }
