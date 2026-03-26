36c5f55b734b8f8a01dcad719f1430f0e870bb67
Barbare997 <barbarepantskhava@gmail.com>
2026-03-26 21:14:46 +0400

    Added public int sumOddLengthSubarrays(int[] arr) {

--- src/MathSolutions.java
+++ src/MathSolutions.java
@@ -372,4 +372,18 @@ public int fib(int n) {
         }
         return result;
     }
+
+
+    //Sum of All Odd Length Subarrays
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        for (int i=0; i<arr.length; i++) {
+            for (int j=i; j<arr.length; j++) {
+                if ((j-i)%2==0)
+                    for (int k=i; k<=j; k++)
+                        sum+=arr[k];
+            }
+        }
+        return sum;
+    }
 }
