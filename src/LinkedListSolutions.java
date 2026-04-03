9edff8bc5f55305e6d6888292feebc1f04d4b44f
Barbare997 <barbarepantskhava@gmail.com>
2026-04-03 16:10:15 +0400

    Added Add Two Numbers solution

--- src/LinkedListSolutions.java
+++ src/LinkedListSolutions.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class LinkedListSolutions {
     //Middle of the Linked List
     public ListNode middleNode(ListNode head) {
@@ -21,5 +23,33 @@ public class LinkedListSolutions {
             fast = fast.next.next;
         }
         return slow;
+    }
+
+    //Add Two Numbers
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        int carry = 0;
+        int curr;
+        ListNode current = result;
+        while (l1 != null || l2 != null) {
+            if (l1 != null && l2 != null)
+                curr = l1.val + l2.val + carry;
+            else curr = Objects.requireNonNullElse(l1, l2).val + carry;
+
+            if (curr >= 10) {
+                carry = curr / 10;
+                curr = curr % 10;
+            } else
+                carry = 0;
+            current.next = new ListNode(curr);
+            current = current.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (carry!=0)
+            current.next = new ListNode(carry);
+        return result.next;
     }
 }
