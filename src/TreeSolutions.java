b6eab1d2665e774f7e0c3ac26fb16aa48ec34f1a
Barbare997 <barbarepantskhava@gmail.com>
2026-05-30 11:47:36 +0400

    Small change in Same Tree solution

--- src/TreeSolutions.java
+++ src/TreeSolutions.java
@@ -10,9 +10,7 @@ public class TreeSolutions {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p==null && q==null)
             return true;
-        else if (p==null)
-            return false;
-        else if (q==null)
+        if (p==null || q==null)
             return false;
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
